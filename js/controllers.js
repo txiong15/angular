cdfeca8e8449f797219562032f1afe66672923e0
Tom Xiong <tomxiong43@msn.com>
2016-01-14 21:26:52 -0600

    added partials

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,8 @@
-/**
- * Created by Tom on 1/13/16.
- */
 
-var myApp = angular.module('myApp', []);
 
-myApp.controller('MyController', ['$scope', '$http', function($scope, $http) {
+var artistControllers = angular.module('artistControllers', []);
+
+artistControllers.controller('ListController', ['$scope', '$http', function($scope, $http) {
 
     //$http.get('js/data.json').then(function () {
     //    $scope.artists = data;
